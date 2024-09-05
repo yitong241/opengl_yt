6af489062770814315602f7b9792f3977537a347
yitong241 <ssunyitong@gmail.com>
2024-09-05 18:23:31 +0800

    Initial Commit

---
+++ opengl_yt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  opengl_yt
+//
+//  Created by Yitong Sun on 5/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
